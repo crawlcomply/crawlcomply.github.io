f03375a34d8f9f582712b5cea570d5a89c808292
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2025-08-28 19:03:50 +0000

    [src/app/landing-page/landing-page.component{.html,.scss,.ts},src/styles.scss] Improve theming
    Triggered by commit: https://github.com/crawlcomply/crawlcomply-www/commit/648161ed781cb77634d58508d681f24a784d7d0a

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 25004, hash: '0846a3ed23d937d00da7b908c22768c9b4dc859d79fbf5c1c6c34fbf5ce12b73', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17459, hash: 'a5b90807ea29ce11b66d49fdfd6916a3c070c7159e05056e3f03ff4081860700', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 97175, hash: 'a6dfb55df9b79aede8a153b74b8849f5a30221d2ec0c887bca978b33e10c42ae', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'styles-DTTV3AOM.css': {size: 8100, hash: 'jHWbwFO0LXY', text: () => import('./assets-chunks/styles-DTTV3AOM_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 69927, hash: '7dc2dc2cffd2586621ba80f481176bfd050230fa71605dcd3564f05164f16a95', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17459, hash: '527668d000657cc89ac420589763e27379dae415cee433f0b28a58b2783886b3', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 153483, hash: '371294ed38b50767f01b36050294d3450f86c5ef88aa0ef21563d33c54dc2119', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'styles-JAGEJZ6K.css': {size: 232712, hash: '6+lX75BFwBs', text: () => import('./assets-chunks/styles-JAGEJZ6K_css.mjs').then(m => m.default)}
   },
 };
