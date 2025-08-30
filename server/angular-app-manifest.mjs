d68cf7ba7069b61ea37a63c832ffaf2001eaf2d2
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2025-08-30 23:31:52 +0000

    [src/app/layout] Refactor to enable easier addition of new pages with consistent layout ; [src/app/faq/faq.html] Add content
    Triggered by commit: https://github.com/crawlcomply/crawlcomply-www/commit/54d9ebdc4b6650ed2295f05220b3f992a4ef4383

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -7,14 +7,25 @@ export default {
   routes: [
   {
     "renderMode": 2,
+    "preload": [
+      "chunk-YW5IWX2O.js"
+    ],
     "route": "/"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-B6SF6P7E.js"
+    ],
+    "route": "/faq"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 69927, hash: '7dc2dc2cffd2586621ba80f481176bfd050230fa71605dcd3564f05164f16a95', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17459, hash: '527668d000657cc89ac420589763e27379dae415cee433f0b28a58b2783886b3', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 153483, hash: '371294ed38b50767f01b36050294d3450f86c5ef88aa0ef21563d33c54dc2119', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'styles-JAGEJZ6K.css': {size: 232712, hash: '6+lX75BFwBs', text: () => import('./assets-chunks/styles-JAGEJZ6K_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 70080, hash: '79b3b4cd5903f652a948d4d6a7327ee6763a2ca838534d48f00519e2c19f287c', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17612, hash: 'e9eba2982e6ec137891847bb41c9c729ba4ad558e1a70c9f7eab75e0787340d8', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 154512, hash: 'f18e1bbedb567e04bff64406323e31d3895422a74f43caca5f645bd0abf952fa', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'faq/index.html': {size: 153832, hash: '271fb873f8fa4bad30a37eebb8a99546e6c575dc32d17669f24f10e4a1d1992e', text: () => import('./assets-chunks/faq_index_html.mjs').then(m => m.default)},
+    'styles-KPVTUMJ5.css': {size: 232865, hash: '2lRaXna3U5U', text: () => import('./assets-chunks/styles-KPVTUMJ5_css.mjs').then(m => m.default)}
   },
 };
