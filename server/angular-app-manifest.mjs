533b81432081acdc6e80ae16bdefeaa98ce10c57
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2025-10-05 05:12:59 +0000

    [src/app/{*.ts,*.html}] Implement simple auth login, auth logout, and local storage store that is Universal (SSR) compliant
    Triggered by commit: https://github.com/crawlcomply/crawlcomply-www/commit/90a160042894e5db3809f9994c49f15f037415fb

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -8,40 +8,39 @@ export default {
   {
     "renderMode": 2,
     "preload": [
-      "chunk-NIR62T23.js"
+      "chunk-GTKFWRNJ.js"
     ],
     "route": "/"
   },
   {
     "renderMode": 2,
     "preload": [
-      "chunk-3HFLXQI4.js"
+      "chunk-EHB4K5LN.js"
     ],
     "route": "/faq"
   },
   {
     "renderMode": 2,
     "preload": [
-      "chunk-IYWENB4O.js"
+      "chunk-UIZ2LMHK.js"
     ],
     "route": "/login"
   },
   {
     "renderMode": 2,
     "preload": [
-      "chunk-OCJMEEH2.js"
+      "chunk-RKV3Q6KI.js"
     ],
     "route": "/profile"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 70286, hash: '89e816433268c38a1fe10c10c52ee12f994562c550a597ed0e668cad3f53727f', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17765, hash: '971464153acc2abf374c75613721194c1f948aef6a95bd43261f553470d15d33', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'login/index.html': {size: 180746, hash: '7d7a58ff1c3531efdc40ce5e761fcf1284b4ecdad6c55cddd6c54ecdf039d3ba', text: () => import('./assets-chunks/login_index_html.mjs').then(m => m.default)},
-    'profile/index.html': {size: 140352, hash: '9797f6808a2adae6c808bbb8094e2a29a374086cade4b5ded5ce3f06e1009e6a', text: () => import('./assets-chunks/profile_index_html.mjs').then(m => m.default)},
-    'faq/index.html': {size: 154539, hash: '3eebe56c68651caf37b3bdfb5ed1665a8bfc593c4eb0c2663576ca6d2b24f8b6', text: () => import('./assets-chunks/faq_index_html.mjs').then(m => m.default)},
-    'index.html': {size: 155219, hash: 'af0b93b2833f1d044e476479d1f5a65726b8845bad3fd609e208d0a4437d1d17', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 70388, hash: 'b1c5f6d873f4f0afe2ddd2b2fff2096dbae36ee462fc8cec9927478874fb69bd', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17867, hash: '0b5d3452ee5cf8fae8e9274420154a1ecdbb93e7fbe183e7517d51255a0b3eee', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 155391, hash: 'e27a6b5e12476111fcfc03eebc6cd89b0f554c335e2e2227d867d90295b551e2', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'login/index.html': {size: 180977, hash: '1b37e85fb21be39c4b5a1899de200ef7f2530907cd4ac9c4000b3996f318b566', text: () => import('./assets-chunks/login_index_html.mjs').then(m => m.default)},
+    'faq/index.html': {size: 154711, hash: '448bc9cb57f3f67be0e9418c2b23f523ec1b3c90d50feb7712d37cc68520720c', text: () => import('./assets-chunks/faq_index_html.mjs').then(m => m.default)},
     'styles-7ZQNNY2E.css': {size: 232919, hash: 'LIOUbJJIpG8', text: () => import('./assets-chunks/styles-7ZQNNY2E_css.mjs').then(m => m.default)}
   },
 };
